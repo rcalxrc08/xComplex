4e45c9c9b3a9abf90a1c00179414acc7ba622ac2
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-02-25 17:45:12 +0100

    Ulteriore Test
    Signed-off-by: rcalxrc08 <nicola.scaramuzzino@yandex.com>

--- test/AComplexDerivativeTest.cpp
+++ test/AComplexDerivativeTest.cpp
@@ -172,10 +172,45 @@ TEST(AComplexDerivative, exp)
 	}
 }
 
-TEST(AComplexDerivative, mixed)
+TEST(AComplexDerivative, mixed1)
 {
 #undef op
 #define op exp(sin(log(cos
+	double aa= (double)rand() / RAND_MAX;
+	double bb= (double)rand() / RAND_MAX;
+	adept::Stack s1;
+	adept::adouble a=aa;
+	adept::adouble b=bb;
+	double a1=aa;
+	double b1=bb;
+	s1.new_recording();
+	acomplex z=acomplex(a,b);
+	std::complex<double> z1=std::complex<double>(a1,b1);
+	double dx=toll;
+	auto Dx=std::complex<double>(dx,0.0);
+	double yDy=(op (z1+Dx))))).real();
+	double y=(op (z1))))).real();
+	double FDDer=(yDy-y)/dx;
+
+	z  =op (z) )));
+	z.getRealP().set_gradient(1.0);
+	s1.reverse();
+	double ADDer=a.get_gradient();
+	double DerToll=toll*1000;
+	std::cout<<"SComplex: "<<std::setprecision(Nprint)<<FDDer<<std::endl;
+	std::cout<<"AComplex: "<<std::setprecision(Nprint)<<a.get_gradient()<<std::endl;
+	EXPECT_NEAR(FDDer, ADDer,DerToll);
+	if (HasFailure())
+	{
+		std::cout<<"SComplex: "<<std::setprecision(Nprint)<<FDDer<<std::endl;
+		std::cout<<"AComplex: "<<std::setprecision(Nprint)<<a.get_gradient()<<std::endl;
+	}
+}
+
+TEST(AComplexDerivative, mixed2)
+{
+#undef op
+#define op exp(sin(sin(sin
 	double aa= (double)rand() / RAND_MAX;
 	double bb= (double)rand() / RAND_MAX;
 	adept::Stack s1;
