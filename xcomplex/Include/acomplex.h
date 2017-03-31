ddda46fc21f73312ba103280fa6b49db415b57eb
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-03-31 15:15:31 +0200

    Merge Ultimato
    Signed-off-by: rcalxrc08 <nicola.scaramuzzino@yandex.com>

--- xcomplex/Include/acomplex.h
+++ xcomplex/Include/acomplex.h
@@ -34,7 +34,7 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os, acomplex& obj)
     {
-        Real tmp1=adept::value(-1.0*obj.getImm());
+        Real tmp1=LVAL (-1.0*obj.getImm()) RVAL;
         if(obj.getImm()>=0.0)
             os<<obj.getReal()<<" + "<<ImagUnit<<" "<<obj.getImm();
         else
